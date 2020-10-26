1e7e374f6e0ef1c8bf03136ebccd6f40ccb94ce7
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-26 13:31:05 -0700

    useEffect hook functional/got data types by console logging request

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from './axios';
+import requests from "./requests";
 
 const Row = ({ title, fetchUrl }) => {
  const [movies, setMovies] = useState( [] );
@@ -7,15 +8,18 @@ const Row = ({ title, fetchUrl }) => {
  // A snippet of code which runs based on a specific condition
  useEffect(() => {
 // run [], once when row loads, and dont run again
+
 async function fetchData(){
-    const request = await axios.get(fetchUrl)
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
 }
 fetchData();
- }, [])
+ }, [fetchUrl])
 
   return (
-    <div>
-      {/*  Title */}
+    <div > 
+ 
       <h2>{title}</h2>
 
       {/* container -> posters  */}
