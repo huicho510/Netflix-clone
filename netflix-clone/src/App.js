15eedd69d8ea31e62735d482c976ce11c38657fe
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-26 14:19:36 -0700

    hid scrollbar/added padding

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests'; 
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-    <h1>front end netflix clone</h1>
-    <Row title= "NETFLIX ORGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-    <Row title= "Trending Now" fetchUrl= {requests.fetchTrending}/>
+      <h1>front end netflix clone</h1>
+      <Row title="NETFLIX ORGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
-import './Row.css';
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -26,13 +26,13 @@ const Row = ({ title, fetchUrl }) => {
       <div className=" row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
           />
         ))}
       </div>
-      {/* container -> posters  */}
     </div>
   );
 };
