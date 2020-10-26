1c02c082ac9c62c9a945b47b33d6c008450a3875
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-26 14:11:43 -0700

    added flex box styling and hover effect

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from "react";
-import axios from './axios';
-import requests from "./requests";
+import axios from "./axios";
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchUrl }) => {
- const [movies, setMovies] = useState( [] );
+  const [movies, setMovies] = useState([]);
 
- // A snippet of code which runs based on a specific condition
- useEffect(() => {
-// run [], once when row loads, and dont run again
+  // A snippet of code which runs based on a specific condition
+  useEffect(() => {
+    // run [], once when row loads, and dont run again
 
-async function fetchData(){
-    const request = await axios.get(fetchUrl);
-    setMovies(request.data.results);
-    return request;
-}
-fetchData();
- }, [fetchUrl])
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
   return (
-    <div > 
- 
+    <div className="row">
       <h2>{title}</h2>
 
+      <div className=" row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/* container -> posters  */}
     </div>
   );
