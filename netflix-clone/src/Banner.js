0775da5d486176acf54f868c3922ef748e868066
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-28 16:54:42 -0700

    created title for header and added styling with image backdrop being rendered

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -10,22 +10,32 @@ function Banner() {
       const request = await axios.get(requests.fetchNetflixOriginals);
       SetMovie(
         request.data.results[
-            Math.floor(Math.random() * request.data.results.length - 1)
-          ]
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
       );
-      return request;  
+      return request;
     }
     fetchData();
   }, []); // runs once when brackets are empty
-  console.log(movie)
+  console.log(movie);
 
   return (
-    <header>
-      {" "}
-      {/*<<< Background Image */}
-      {/* title */}
-      {/* div > 2 buttons */}
-      {/* description */}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+            )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        {/* title */}
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        {/* div > 2 buttons */}
+        {/* description */}
+      </div>
     </header>
   );
 }
