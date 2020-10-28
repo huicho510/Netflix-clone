7ebec12837129b68e0b436e371f8baf0527dab3d
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-28 16:37:24 -0700

    Created useEffect/async function to make request with axios/console.log is working

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 function Banner() {
-    return (
-        <header> {/*<<< Background Image */}
-           {/* title */} 
-           {/* div > 2 buttons */} 
-           {/* description */} 
+  const [movie, SetMovie] = useState([]);
 
-            
-        </header>
-    )
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      SetMovie(
+        request.data.results[
+            Math.floor(Math.random() * request.data.results.length - 1)
+          ]
+      );
+      return request;  
+    }
+    fetchData();
+  }, []); // runs once when brackets are empty
+  console.log(movie)
+
+  return (
+    <header>
+      {" "}
+      {/*<<< Background Image */}
+      {/* title */}
+      {/* div > 2 buttons */}
+      {/* description */}
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
