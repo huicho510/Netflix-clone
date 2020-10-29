45c6c2524b5687b13c88a6a07e70bbc367fa6cf2
Luis Cervantes <luiscerv1229@gmail.com>
2020-10-29 16:53:35 -0700

    added styling to banner to match netflix page sizes

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, SetMovie] = useState([]);
@@ -19,6 +20,11 @@ function Banner() {
   }, []); // runs once when brackets are empty
   console.log(movie);
 
+  function truncate(str, n) {
+      return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+
   return (
     <header
       className="banner"
@@ -32,12 +38,14 @@ function Banner() {
     >
       <div className="banner__contents">
         {/* title */}
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My list</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
       </div>
     </header>
   );
