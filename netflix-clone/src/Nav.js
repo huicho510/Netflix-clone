014721d2fd344d16ae26ed095b84cf35ef45996a
Luis Cervantes <luiscerv1229@gmail.com>
2020-11-02 12:18:10 -0800

    added some state and effect to nav bar with a scroll event listener/adds some styling to navbar when scrolling more than 100px

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -1,10 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css';
 
 function Nav() {
+    const [show, handleShow] = useState()
 
+    useEffect(  () => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav__black"}`}>
             <img
             className= "nav__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png"
