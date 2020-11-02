8fbbd80edd15df10ca86f0ef724acb40d08d958d
Luis Cervantes <luiscerv1229@gmail.com>
2020-11-02 13:22:31 -0800

    final push with build and deployment

--- netflix-clone/src/axios.js
+++ netflix-clone/src/axios.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 /** base url to make requests to the movie database */
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+  baseURL: "https://api.themoviedb.org/3",
 });
-
-instance.get()
-
 
 export default instance;
