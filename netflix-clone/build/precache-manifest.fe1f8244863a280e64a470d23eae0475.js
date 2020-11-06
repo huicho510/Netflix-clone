f2b8cb688464e87aa7ee5cb53f3e16989ba5f23b
Luis Cervantes <luiscerv1229@gmail.com>
2020-11-06 12:02:47 -0800

    fixed deployment

---
+++ netflix-clone/build/precache-manifest.fe1f8244863a280e64a470d23eae0475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9182c12862e3862a5ce86a4a01872cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254acf0e88abdb5edae1",
+    "url": "/static/css/main.9063aa5f.chunk.css"
+  },
+  {
+    "revision": "7b7126e4fd4b5d04b221",
+    "url": "/static/js/2.92874c65.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/static/js/2.92874c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254acf0e88abdb5edae1",
+    "url": "/static/js/main.67353d19.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
